fa4439a159ac3b56fc2b43f3e9eff7148ed17172
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-20 21:40:05 +0900

    remove unused code

--- bin/dart_server.dart
+++ bin/dart_server.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
 
 //lib
 import 'package:dart_server/dart_server.dart';
 import 'controller/welcome_controller.dart';
-
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -16,9 +13,6 @@ bool isServerOpened = false;
 
 void main(List<String> arguments) async {
   final router = Router();
-
-  final file = File('bin/frontend/index.html');
-  final fileData = await file.readAsString();
 
   // router.get('/',(Request request){
   //   return Response.ok(fileData,headers:{'Content-Type':"text/html; charset=utf-8"});

bb86d3c27ad6aeb0a4719559e4e9263e6e79fadc
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-20 19:33:15 +0900

    implement all http method contructor for router

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,14 +1,31 @@
 import 'dart:io';
 
 import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
 
-abstract class Controller{
+class Controller{
 
   Controller(String url);
 
+  Controller.regist(String url,Router router){
+    router.get(url,get);
+    router.head(url,head);
+    router.post(url,post);
+    router.put(url,put);
+    router.delete(url, delete);
+    router.connect(url,connect);
+    router.options(url,options);
+    router.trace(url,trace);
+    router.patch(url, patch);
+  }
+
   late final String url;
 
-  Response get() {
+  Response get(Request request) {
+    return Response(HttpStatus.notImplemented);
+  }
+
+  Response head() {
     return Response(HttpStatus.notImplemented);
   }
 
@@ -16,7 +33,23 @@ abstract class Controller{
     return Response(HttpStatus.notImplemented);
   }
 
+  Response put() {
+    return Response(HttpStatus.notImplemented);
+  }
+
   Response delete() {
+    return Response(HttpStatus.notImplemented);
+  }
+
+  Response connect() {
+    return Response(HttpStatus.notImplemented);
+  }
+
+  Response options() {
+    return Response(HttpStatus.notImplemented);
+  }
+
+  Response trace() {
     return Response(HttpStatus.notImplemented);
   }
   

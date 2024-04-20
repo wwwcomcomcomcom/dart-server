06acc67e63b909f2e7fbe756120d2b9a76d3e276
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-20 22:03:11 +0900

    test dart `html` parser package

--- bin/controller/welcome_controller.dart
+++ bin/controller/welcome_controller.dart
@@ -1,5 +1,8 @@
 import "package:dart_server/controller.dart";
+import "package:html/parser.dart";
 import "package:shelf/shelf.dart";
+// import 'dart:html';/
+
 
 
 class WelcomeController extends Controller {
@@ -7,9 +10,9 @@ class WelcomeController extends Controller {
   WelcomeController(super.url);
 
   WelcomeController.regist(super.url, super.router) : super.regist();
-
   @override
   Response get(Request request){
-    return Response.ok("Hello");
+    var component = parse("<h1>template?<h1>",encoding: "UTF-8");
+    return Response.ok("Hello ${component.outerHtml}");
   }
 }

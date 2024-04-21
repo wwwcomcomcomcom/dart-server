d6d1a23f403ff5a7945f68715dbe07c83b2d581c
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-21 18:19:18 +0900

    use template value as tmp-text

--- lib/view/template.dart
+++ lib/view/template.dart
@@ -9,18 +9,14 @@ import 'package:html/parser.dart' as HtmlParser;
 
 const prototypeHTML = 'bin/frontend/template_prototype.html';
 
-enum Command {
-  show,
-  delete,
-  error
-}
+enum Command { show, delete, error }
 
 class Model {
-  final Map<String,dynamic> data;
+  final Map<String, dynamic> data;
 
   Model(this.data);
 
-  dynamic readModel(String path){
+  dynamic readModel(String path) {
     List<String> keyPath = path.split(".");
     dynamic currentValue = data;
     for (String key in keyPath) {
@@ -31,48 +27,54 @@ class Model {
   }
 }
 
+void spreadElements(Element element) {
 
-void spreadElements(Element element){
+  final Model model = Model({
+    "test": {
+      "result": true,
+      "value":"HELLOWORLD"
+    }
+  });
+
   // ignore: no_leading_underscores_for_local_identifiers
   element.attributes.forEach((_key, value) {
     var key = _key.toString();
-    if(key.startsWith("*")){
+    if (key.startsWith("*")) {
       final rawCommand = key.substring(1);
-      final command = parseCommand(rawCommand, value,Model({
-        "test":{
-          "result":true
-        }
-        }));
+      final command = parseCommand(rawCommand, value, model);
       print(command);
     }
   });
-  if(element.children.isNotEmpty){
+  if (element.localName == "tmp-text") {
+    element.innerHtml = model.readModel(element.innerHtml).toString();
+    print(element.innerHtml);
+  }
+
+  print(element.localName);
+  if (element.children.isNotEmpty) {
     element.children.forEach(spreadElements);
   }
 }
 
-Command parseCommand (String command,String arg,Model model){
-
+Command parseCommand(String command, String arg, Model model) {
   dynamic value = model.readModel(arg);
 
   switch (command) {
     case "if":
-      if(value is bool && value){
+      if (value is bool && value) {
         return Command.show;
-      }else{
+      } else {
         return Command.delete;
       }
-      // break;
+    // break;
     default:
       return Command.error;
   }
 }
 
-void executeCommand(){
+void executeCommand() {}
 
-}
-
-String parseTemplate(){
+String parseTemplate() {
   final file = File(prototypeHTML);
   final fileData = file.readAsStringSync();
   final component = HtmlParser.parse(fileData);
@@ -82,9 +84,12 @@ String parseTemplate(){
   // html.children[1].children[1].attributes.forEach((key, value) {
   //   key = key.toString();
   //   print("key : $key value: $value");
-    
+
   // },);
 
   // print(component);
-  return fileData;
+
+  //TODO: make stringify
+  
+  return component.outerHtml;
 }

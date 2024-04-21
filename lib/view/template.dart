8833a582f0a51c8a865b7a9cb2651be9341250ce
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-21 12:52:31 +0900

    read and spread html elements

--- lib/view/template.dart
+++ lib/view/template.dart
@@ -3,13 +3,32 @@
 //Todo:
 
 import 'dart:io';
+import 'package:html/dom.dart';
 // ignore: library_prefixes
 import 'package:html/parser.dart' as HtmlParser;
 
+const prototypeHTML = 'bin/frontend/template_prototype.html';
+
+void spreadElements(Element element){
+  print("${element.localName}");
+  if(element.children.isNotEmpty){
+    element.children.forEach(spreadElements);
+  }
+}
+
 String parseTemplate(){
-  final file = File('bin/frontend/index.html');
+  final file = File(prototypeHTML);
   final fileData = file.readAsStringSync();
-  var component = HtmlParser.parse(fileData);
-  print(component);
+  final component = HtmlParser.parse(fileData);
+  //html body h2
+  final html = component.children[0];
+  spreadElements(html);
+  // html.children[1].children[1].attributes.forEach((key, value) {
+  //   key = key.toString();
+  //   print("key : $key value: $value");
+    
+  // },);
+
+  // print(component);
   return fileData;
 }

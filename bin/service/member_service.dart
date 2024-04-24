db7e5feaf612a2d76a67051442ad4e097357fd1b
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-24 19:53:53 +0900

    implement MemberService

---
+++ bin/service/member_service.dart
@@ -0,0 +1,27 @@
+import '../model/member.dart';
+import '../repository/member_repo.dart';
+import '../repository/memory_member_repo.dart';
+
+class MemberService {
+  late final MemberRepository _memberRepository;
+
+  MemberService(MemberRepository memberRepository);
+
+  int join(Member member){
+    checkDuplicateMember(member);
+    _memberRepository.save(member);
+    return member.id;
+  }
+
+  void checkDuplicateMember(Member member){
+    if(_memberRepository.findByName(member.name) != null) throw Exception("Illegal State Exception");
+  }
+
+  List<Member> findMembers(){
+    return _memberRepository.findAll();
+  }
+
+  Member? fineOne(int id){
+    return _memberRepository.findById(id);
+  }
+}

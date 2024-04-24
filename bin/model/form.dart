b68d44b44d6cbdbf90dccbf141865a64ad393ea7
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-24 19:51:55 +0900

    implement form model

---
+++ bin/model/form.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: unnecessary_getters_setters
+
+class MemberForm {
+  late String _name;
+
+  MemberForm(String name);
+
+  String get name => _name;
+
+  set name(String name) => _name = name;
+}

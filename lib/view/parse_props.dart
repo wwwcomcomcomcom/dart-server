d8f306f4be6ca8097f084156736c2f2d50b7bc99
wwwcomcomcomcom <iieiiergn@gmail.com>
2024-04-25 19:44:36 +0900

    define about `*foreach`

--- lib/view/parse_props.dart
+++ lib/view/parse_props.dart
@@ -14,3 +14,16 @@ void executeIfProp(Element element,String key, Model model){
     element.removeSelf();
   }
 }
+
+void executeForeachProp(Element element,String key, Model model){
+  final pathValue = element.attributes[key];
+  if(pathValue == null) return;
+  if(element.parent == null) return;
+  dynamic data = model.readData(pathValue);
+  if(data is! Iterable) return;
+  for (var value in (data as Iterable)) {
+    final clone = element.clone(true);
+    clone.attributes.remove(key);
+    element.parent!.append(clone);
+  }
+}
